acaf09ee00857412af488e78cfcc1aa6fc73fd6c
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-10-26 21:01:48 -0400

    update deploy script, setup env vars

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -3,11 +3,34 @@ import { ethers } from "hardhat";
 async function main() {
   // We get the contract to deploy
   const gameContractFactory = await ethers.getContractFactory("MyEpicGame");
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    ["Gilgamesh", "Rem", "Jpop"], // Names
+    [
+      "https://wallpapercave.com/wp/wp2085349.jpg", // Images
+      "https://i.redd.it/strg79a0b1611.png",
+      "https://media-exp1.licdn.com/dms/image/C4D03AQEJtGbbESjEQA/profile-displayphoto-shrink_800_800/0/1525376249105?e=1640822400&v=beta&t=9zpiZXIrnjFR_Fh8Ss1U_4-7MNIlzfkdRUXeE31BdKA",
+    ],
+    [100, 200, 1], // HP values
+    [150, 100, 5000] // Attack damage values
+  );
 
   await gameContract.deployed();
 
   console.log("Contract deployed to:", gameContract.address);
+
+  let tx = await gameContract.mintCharacterNFT(0);
+  await tx.wait();
+  console.log("Minted NFT #1");
+
+  tx = await gameContract.mintCharacterNFT(1);
+  await tx.wait();
+  console.log("Minted NFT #2");
+
+  tx = await gameContract.mintCharacterNFT(2);
+  await tx.wait();
+  console.log("Minted NFT #3");
+
+  console.log("Done deploying and minting!");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
