6b67cef9beca52b04583f1e2fb580656b0893cd7
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-10-26 20:51:15 -0400

    override tokenURI function in contract

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -10,6 +10,9 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "hardhat/console.sol";
 
+import "./libraries/Base64.sol";
+
+
 contract MyEpicGame is ERC721 {
     struct CharacterAttributes {
       uint characterIndex;
@@ -80,5 +83,36 @@ contract MyEpicGame is ERC721 {
 
       // Increment the tokenId for the next person that uses it.
       _tokenId.increment();
+    }
+
+    function tokenURI(uint256 _tokenID) public view override returns (string memory) {
+      CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenID];
+
+      string memory strHp = Strings.toString(charAttributes.hp);
+      string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+      string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+      string memory json = Base64.encode(
+        bytes(
+          string(
+            abi.encodePacked(
+              "{\"name\": \"",
+              charAttributes.name,
+              " -- NFT #: ",
+              Strings.toString(_tokenID),
+              "\", \"description\": \"This is an NFT that lets people play in the game Metaverse Slayer!\", \"image\": \"",
+              charAttributes.imageURI,
+              "\", \"attributes\": [ { \"trait_type\": \"Health Points\", \"value\": ",strHp,", \"max_value\":",strMaxHp,"}, { \"trait_type\": \"Attack Damage\", \"value\": ",
+              strAttackDamage,"} ]}"
+            )
+          )
+        )
+      );
+
+      string memory output = string(
+        abi.encodePacked("data:application/json;base64,", json)
+      );
+      
+      return output;
     }
 }
